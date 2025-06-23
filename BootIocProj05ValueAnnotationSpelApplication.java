0dca553b5de1b4730c0cb4a9bcb965b1e1babd21
jshrghosh <jshrghosh80@gmail.com>
2025-06-23 21:13:03 +0530

    Proj 3 of @Value using Special Expression Language

---
+++ BootIocProj05ValueAnnotationSpelApplication.java
@@ -0,0 +1,19 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.nt.sbeans.Hotel;
+
+@SpringBootApplication
+public class BootIocProj05ValueAnnotationSpelApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx= SpringApplication.run(BootIocProj05ValueAnnotationSpelApplication.class, args);
+		Hotel hotel=ctx.getBean("hotel",Hotel.class);
+		System.out.println(hotel);
+		((ConfigurableApplicationContext)ctx).close();
+    }
+}
